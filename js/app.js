b6466805704f4dc3a7b1c4639d26d5194704a7fe
bhan <bhan@xiaohongshu.com>
2017-02-13 23:45:58 +0800

    guard and vendor

---
+++ js/app.js
@@ -0,0 +1,41 @@
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives',
+  'starter.services'
+])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+});
+
+angular.module('starter')
+  .filter('toTimeStamp', function() {
+    return function(input, param) {
+      return moment(input).unix() * 1000;
+    }
+  });
+
+angular.module('starter.directives', [])
+  .directive('goBack', function() {
+    return {
+      restrict: 'A',
+      replace: true,
+      template: '<div class="back-wrap" ng-click="myGoBack()"> ' +
+        '<i class="ion-arrow-left-c"></i><span>返回</span>' + '</div>',
+      controller: function($scope, $state, $ionicHistory) {
+        $scope.myGoBack = function() {
+          $backView = $ionicHistory.backView();
+          if ($backView) {
+            $backView.go();
+          } else {
+            $state.go('app.sessions')
+          }
+        };
+      }
+    }
+  })
+
+;
---
+++ js/userInfo.js
@@ -0,0 +1,51 @@
+angular.module('starter.services', ['ngResource'])
+
+.factory('userWechatInfo', function($resource) {
+  return $resource('http://www.lifeuxuan.com/index.php/user/basicinfo');
+})
+
+.factory('userRegister', function($resource) {
+  return $resource('http://www.lifeuxuan.com/index.php/user/register');
+})
+
+.factory('UserInfo', function($resource, $q, $timeout, userWechatInfo,
+  userRegister) {
+  var deferred = $q.defer();
+  var user = {}
+
+  // ------------for test-----------------
+  if (window.location.hostname == "localhost") {
+    deferred.resolve(user);
+    return deferred.promise;
+  }
+  // ------------for test-----------------
+
+  userWechatInfo.get({}, function(e) {
+    user.name = e.data.nickname;
+    user.img = e.data.headimgurl;
+    user.openid = e.data.openid;
+    user.headPicUrl = e.data.headimgurl;
+    userRegister.get({
+      'latitude': user.latitude,
+      'longitude': user.longitude,
+      'openId': user.openid,
+      'username': user.nickname,
+      'password': '',
+      'headPicUrl': user.headPicUrl
+    }, function(e) {
+      if (e.data) {
+        user.userId = e.data.userId;
+        user.verify = e.data.verifyCode;
+        var address = e.data.lastAddress;
+        user.rcvAddress = address.rcvAddress;
+        user.rcvPhone = address.rcvPhone;
+        user.rcvName = address.rcvName;
+        deferred.resolve(user);
+      }
+    })
+  });
+
+  return deferred.promise;
+})
+
+;
